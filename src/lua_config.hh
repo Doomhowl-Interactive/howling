084def7d40bb9f2a64a85337109564f06d8d50b4
bramtechs <bramtech@telenet.be>
2024-10-07 18:11:38 +0200

    ordered vector + rename lua_config methods

--- src/lua_config.hh
+++ src/lua_config.hh
@@ -1,6 +1,8 @@
 #pragma once
 #include "lua_machine.hh"
+
 #include <string>
+#include <cassert>
 
 namespace HOWLING_NAMESPACE
 {
@@ -12,18 +14,21 @@ public:
     ~LuaConfig();
 
     template<typename T>
-    T getConfigValue(const std::string& name, T defaultValue)
-    {
-        return state["config"][name].get_or<T>(defaultValue);
+    [[nodiscard]] static T getValue(const std::string& name, T defaultValue = {}) {
+        return ins().state["config"][name].get_or<T>(defaultValue);
     }
 
-    bool getConfigBool(const std::string& name)
-    {
-        return getConfigValue<bool>(name, false);
+    [[nodiscard]] static bool getBool(const std::string& name) {
+        return ins().getValue<bool>(name, false);
     }
 
 private:
     static inline LuaConfig* sInstance {};
+
+    static LuaConfig& ins() {
+        assert(sInstance);
+        return *sInstance;
+    }
 };
 
 }
